12ea483ed568fbc4b01ec735cbcbd2f3c26b7931
fred-machuga <sita.machuga@gmail.com>
2026-04-15 11:00:26 -0400

    fix: Update python tests to match Freds manual rust bindings update

--- engine/src/bindings.rs
+++ engine/src/bindings.rs
@@ -4,39 +4,48 @@
 use pyo3::prelude::*;
 use crate::orbital::OrbitState;
 
-#[pyclass(get_all)]
-#[derive(Clone, Debug)]
-pub struct PyOrbitalElements {
-    pub semi_major_axis: f64,
-    pub eccentricity: Option<f64>,
-    pub inclination: Option<f64>,
-    pub longitude_of_ascending_node: Option<f64>,
-    pub argument_of_periapsis: Option<f64>,
-    pub mean_anomaly: Option<f64>,
-}
-
 #[pyclass(module = "delta_wrecker.orbital")]
 pub struct PyOrbitState {
     state: OrbitState,
-    #[pyo3(get)]
-    pub time: f64,
 }
 
 #[pymethods]
 impl PyOrbitState {
     #[new]
-    pub fn new(x: f64, y: f64, z: f64, vx: f64, vy: f64, vz: f64, time: f64) -> Self {
+    pub fn new(x: f64, y: f64, z: f64, vx: f64, vy: f64, vz: f64) -> Self {
         Self {
             state: OrbitState::new(x, y, z, vx, vy, vz),
-            time,
         }
     }
 
-    pub fn propagate(&self, dt_s: f64) -> PyOrbitState {
-        PyOrbitState {
-            state: crate::orbital::propagate(&self.state, dt_s),
-            time: self.time + dt_s,
-        }
+    #[getter]
+    pub fn x(&self) -> f64 {
+        self.state.x
+    }
+
+    #[getter]
+    pub fn y(&self) -> f64 {
+        self.state.y
+    }
+
+    #[getter]
+    pub fn z(&self) -> f64 {
+        self.state.z
+    }
+
+    #[getter]
+    pub fn vx(&self) -> f64 {
+        self.state.vx
+    }
+
+    #[getter]
+    pub fn vy(&self) -> f64 {
+        self.state.vy
+    }
+
+    #[getter]
+    pub fn vz(&self) -> f64 {
+        self.state.vz
     }
 
     #[getter]
@@ -49,42 +58,28 @@ impl PyOrbitState {
         (self.state.vx, self.state.vy, self.state.vz)
     }
 
-    #[getter]
-    pub fn orbital_elements(&self) -> PyOrbitalElements {
-        let r = self.state.distance_km();
-        let v_mag = self.state.speed_kms();
-        let v2 = v_mag * v_mag;
-        let gm = crate::orbital::MU_EARTH;
-        let a = 1.0 / (2.0 / r - v2 / gm);
+    pub fn altitude_km(&self) -> f64 {
+        self.state.altitude_km()
+    }
 
-        PyOrbitalElements {
-            semi_major_axis: a,
-            eccentricity: None,
-            inclination: None,
-            longitude_of_ascending_node: None,
-            argument_of_periapsis: None,
-            mean_anomaly: None,
+    pub fn speed_kms(&self) -> f64 {
+        self.state.speed_kms()
+    }
+
+    pub fn distance_km(&self) -> f64 {
+        self.state.distance_km()
+    }
+    
+    pub fn propagate(&self, dt_s: f64) -> PyOrbitState {
+        PyOrbitState {
+            state: self.state.propagate(dt_s)
         }
     }
-}
-
-/// A simple function that adds two numbers, exposed to Python.
-#[pyfunction]
-fn add_numbers(a: i32, b: i32) -> PyResult<i32> {
-    Ok(a + b)
-}
-
-/// A function that returns a greeting from Rust.
-#[pyfunction]
-fn hello_from_rust() -> PyResult<String> {
-    Ok("Hello from Rust!".to_string())
 }
 
 /// The Python module definition.
 #[pymodule]
 fn delta_wrecker(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(add_numbers, m)?)?;
-    m.add_function(wrap_pyfunction!(hello_from_rust, m)?)?;
     m.add_class::<PyOrbitState>()?;
     Ok(())
 }
